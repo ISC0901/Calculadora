9cdfc4825f5b6eb761fa53f25629346ce6b8890f
ISC0901 <166071589+ISC0901@users.noreply.github.com>
2025-05-12 17:02:26 -0300

    Update receber.php

--- receber.php
+++ receber.php
@@ -1,4 +1,28 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head><style>
+    body {
+      font-family: Arial, sans-serif;
+      background-color:rgba(179, 179, 179, 0.64);
+      padding: 20px;
+      justify-content: center; /* centraliza horizontalmente */
+      align-items: center;     /* centraliza verticalmente */
+          
+    }
+    .centralizado {
+      width: 80%;
+      max-width: 800px;
+      text-align: center;
+      padding: 20px;
+      background-color:rgb(247, 59, 190);
+      border-radius: 10px;
+      box-shadow: 0 0 10px rgb(253, 246, 246);
+    }
+    </style>
+    </head>
+    <body> 
+ <center>   <div class="centralizado">
+     <h1>   <?php
  
 
 $n1=$_POST['numero1'] ;
@@ -6,17 +30,17 @@ $n2=$_POST['numero2'];
 $op=$_POST['op'];
 
 if($op=="+")
- echo "resultado da soma  ".$n1+$n2."<br>";
+ echo "Resultado da soma  ".$n1+$n2."<br>";
  if($op=="-")
- echo "resultado da subtração    ".$n1-$n2."<br>";
+ echo "Resultado da subtração    ".$n1-$n2."<br>";
  if($op=="*")
- echo "resultado  da multiplicação  ".$n1*$n2."<br>";
+ echo "Resultado  da multiplicação  ".$n1*$n2."<br>";
  if($op=="/")
- echo "resultado da divisão  ".$n1/$n2."<br>" ;
+ echo "Resultado da divisão  ".$n1/$n2."<br>" ;
 
  $total = $n1+$n2;
 
- echo"<h2>operação escolhida</h2>", $op."<br>";
+ echo"<h2>Operação escolhida</h2>", $op ."<br>";
 
  echo " NUMERO ".$n1."<br>";
 
@@ -24,3 +48,9 @@ if($op=="+")
 
 
 ?>
+</h1>
+
+   
+    <button onclick="location.href='index.php'">Ir para a pagina anterior </button>
+    </div></center> </body>
+</html>
